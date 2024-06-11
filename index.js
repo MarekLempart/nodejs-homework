5f19df9867016ad22c90152fa6543d1beb37e9f2
MarekLempart <kama.zakupy@gmail.com>
2024-06-11 20:17:49 +0200

    Przygotowanie repozytorium do prac domowych

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("Aby rozpocząć pracę z homework:");
+console.log("- wpisz w konsli 'npm run',");
+console.log("- wybierz odpowiedni homework,");
+console.log("- wywołaj go przez wpisanie w konspli 'npm run nazwa_homework'.");
